7734523d074c4edeb22e87057832e7700c639a7a
lushdog <lushdog@62f4d935-ece3-4f1a-ac47-83beac009040>
2009-02-15 04:05:21 +0000

    Last checkin added rudimentary animation to Sprite class and its children.  All textures are loaded from the SpriteSheet class.
    git-svn-id: https://lulz.unfuddle.com/svn/lulz_repos@34 62f4d935-ece3-4f1a-ac47-83beac009040

--- Trunk/MyFirstGame/Game1.cs
+++ Trunk/MyFirstGame/Game1.cs
@@ -62,7 +62,7 @@ namespace MyFirstGame
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+            /// Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             
             // TODO: use this.Content to load your game content here
